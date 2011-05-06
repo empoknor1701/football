a768da5c99945e04df295686203b60f3c27b20ec
fourside <fourside@gmail.com>
2011-05-06 15:23:09 +0900

    add sql script for insert data to Team

---
+++ src/sql/insertToTeam.sql
@@ -0,0 +1,6 @@
+INSERT INTO TEAM(name) VALUE('Manchester FC');
+INSERT INTO TEAM(name) VALUE('London FC');
+INSERT INTO TEAM(name) VALUE('Liverpool FC');
+INSERT INTO TEAM(name) VALUE('Blackburn FC');
+INSERT INTO TEAM(name) VALUE('Leeds FC');
+INSERT INTO TEAM(name) VALUE('Ipswich FC');
