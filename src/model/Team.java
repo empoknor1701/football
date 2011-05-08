445f6b00bdb361e6f4259c9d7aad59cbd917a647
fourside <fourside@gmail.com>
2011-05-08 12:28:39 +0900

    create player.Team class

---
+++ src/model/Team.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Team {
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
