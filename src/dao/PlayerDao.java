ad7b41332e3821a6db7fd24f6537d4e7cb1bd427
fourside <fourside@gmail.com>
2011-05-08 14:19:51 +0900

    add method : getPlayerList

--- src/dao/PlayerDao.java
+++ src/dao/PlayerDao.java
@@ -1,9 +1,12 @@
 package dao;
 
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
 
 import model.Player;
 
 public interface PlayerDao {
 	void insertPlayer(Player player) throws DataAccessException;
+	List getPlayerList(Integer teamId);
 }
