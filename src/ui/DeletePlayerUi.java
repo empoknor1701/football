3280ec0ead4e955950661620fb474a7847d12e27
fourside <fourside@gmail.com>
2011-05-08 20:52:16 +0900

    create class DeletePlayerUi

---
+++ src/ui/DeletePlayerUi.java
@@ -0,0 +1,43 @@
+package ui;
+
+
+import model.Player;
+
+import org.apache.commons.lang.StringUtils;
+
+import dao.PlayerDao;
+
+public class DeletePlayerUi extends AbstractUi {
+
+	private PlayerDao playerDao;
+	
+	@Override
+	public void show() {
+		this.showMenu();
+		String id = this.getInputedString();
+		if (StringUtils.isEmpty(id)) {
+			return;
+		} else if (UiUtils.isNumeric(id, "選手ID")) {
+			Player player = this.playerDao.getPlayer(Integer.parseInt(id));
+			if (player == null) {
+				System.out.println("入力された選手ID「" + id + "」の選手は存在しません。");
+				this.show();
+			} else {
+				this.playerDao.deletePlayer(player);
+				System.out.println("選手ID「" + id + "」の選手を削除しました。");
+			}
+		} else {
+			this.show();
+		}
+		
+	}
+
+	private void showMenu() {
+		System.out.println("=========================");
+		System.out.println("選手名鑑: 選手削除");
+		System.out.println("");
+		System.out.println("選手IDを入力し、ENTERを押してください。");
+		System.out.println("ENTERの空打ちでメニューに戻ります。");
+	}
+
+}
